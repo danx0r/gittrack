5516938554a8927b51cc29e912ebd02484b929df
danx0r <danbmil99@gmail.com>
2015-01-03 20:05:35 -0800

    own it

---
+++ config_example.py
@@ -0,0 +1,8 @@
+#define aliases to mask plain-text passwords
+user = {
+    'nickname': {
+        'user': 'realgithubusername',
+        'pw': {
+            'plaintextpassword': 'realpassword'
+        }
+    }
